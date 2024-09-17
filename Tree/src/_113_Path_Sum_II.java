0830170c5c772e432de23f6eccc8331124162c55
DangDangHoccOdE <haidang972004@gmail.com>
2024-09-17 23:31:32 +0700

    push

--- Tree/src/_113_Path_Sum_II.java
+++ Tree/src/_113_Path_Sum_II.java
@@ -16,18 +16,25 @@ public class _113_Path_Sum_II {
     public static List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         List<List<Integer>> list = new ArrayList<>();
 
+        List<Integer> currPath = new ArrayList<>();
+        dfs(list,currPath,targetSum,root);
         return list;
     }
 
-    public void duyet(List<List<Integer>> list,int target,TreeNode root,int count){
+    public static void dfs(List<List<Integer>> list,List<Integer> currentPath, int target, TreeNode root){
           if(root == null){
               return;
           }
-          count+=root.val;
-          duyet(list,target,root.left,count);
-          if(count == target){
-
+          currentPath.add(root.val);
+          if(root.left == null && root.right == null && target == root.val){
+              list.add(new ArrayList<>(currentPath));
+          }else{
+              dfs(list,currentPath,target-root.val,root.left);
+              dfs(list,currentPath,target-root.val,root.right);
           }
 
+          // Back tracking
+
+        currentPath.remove(currentPath.size() - 1);
     }
 }
